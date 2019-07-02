f139867a101ccce5cf91416530c413b3ba36add5
ZtyWyh <872690952@qq.com>
2019-07-02 22:08:15 +0800

    变态跳台阶

---
+++ BeefMan/变态跳台阶.cpp
@@ -0,0 +1,20 @@
+// 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+
+
+// f(n) = f(n-1)+f(n-2)+...+f(n-(n-1)) + f(n-n) => f(0) + f(1) + f(2) + f(3) + ... + f(n-1)
+// f(n-1) = f(0) + f(1)+f(2)+f(3) + ... + f((n-1)-1) = f(0) + f(1) + f(2) + f(3) + ... + f(n-2)
+//  f(n) = f(0) + f(1) + f(2) + f(3) + ... + f(n-2) + f(n-1) = f(n-1) + f(n-1)
+// 所以 f(n) = 2*f(n-1)
+
+class Solution {
+public:
+    int jumpFloorII(int number) {
+        if(number <= 0){
+            return -1;
+        }else if(number == 1){
+            return 1;
+        }else{
+            return 2 * jumpFloorII(number - 1);
+        }
+    }
+};
